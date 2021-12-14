3211f0f50d1504a9b3171df937c0ea5e190d57d7
dannyanderson98 <78023150+dannyanderson98@users.noreply.github.com>
2021-12-14 20:09:13 +0000

    Add files via upload

---
+++ BrainStormer_Quiz_Project/Quizzing.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/string:page_name>/')
+def render_static(page_name):
+	 return render_template(‘Brain_Stormer.html’)
+if __name__ == '__main__':
+	app.run()
+
+
+
+	
+
+
+
+
+
+
+
