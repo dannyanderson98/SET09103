b5a2058f2e144eaa38ce4d18e4fc7ad577f2fc75
dannyanderson98 <78023150+dannyanderson98@users.noreply.github.com>
2022-01-12 12:48:33 +0000

    Add files via upload

---
+++ SET09103_Quiz_Project/BRAIN_STORMERQUIZ.py
@@ -0,0 +1,16 @@
+from flask import Flask, current_app
+app = Flask(__name__)
+
+@app.route('/')
+def SET09103_Quiz_Project():
+    return current_app.send_static_file('Brain_Stormer.html')
+
+if __name__=='__name__':
+	app.run()
+	
+
+
+
+
+
+
